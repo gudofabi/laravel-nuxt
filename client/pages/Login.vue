7a8adafe9c121b9fb9bde6b9d71c3a7c43ea75dc
gudofabi <godofredocpanel@gmail.com>
2021-10-31 18:39:29 +0800

    fix: initial commit

---
+++ client/pages/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <a href="#" @click.prevent="func_login">Login</a>
+</template>
+
+<script>
+export default {
+ methods: {
+		func_login() {
+			this.$auth.loginWith('laravelSanctum', {
+				data: {
+					email: 'godofredocpanel@gmail.com',
+					password: 'password'
+				}
+			})
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
